51034c668e74c6dc900e5608951c713ed3fbc5ed
Lux-source <andrei-lucian@hotmail.com>
2025-03-06 16:36:40 +0100

    traducciones 2/3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,11 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://porfolio.dev/',
+  vite: {
+    define: {
+      'process.env.LANGUAGES': JSON.stringify(['es', 'en']),
+      'process.env.DEFAULT_LANGUAGE': JSON.stringify('es')
+    }
+  }
 })
--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,23 +1,13 @@
-// src/utils/i18n.js
 import es from '../../local/es.json';
 import en from '../../local/en.json';
 
 const translations = { es, en };
 
-export const getLang = () => {
-    // Si localStorage no está disponible (SSR), se retorna por defecto "es"
-    if (typeof localStorage !== 'undefined') {
-      return localStorage.getItem('lang') || 'es';
-    }
-    return 'es';
-  };
-  
-  export const t = (key) => {
-    const lang = getLang();
-    const keys = key.split('.');
-    let value = translations[lang];
-    keys.forEach((k) => {
-      if (value) value = value[k];
-    });
-    return value || key;
-  };
+export const t = (lang, key) => {
+  const keys = key.split('.');
+  let value = translations[lang];
+  keys.forEach((k) => {
+    if (value) value = value[k];
+  });
+  return value || key;
+};
